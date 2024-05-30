fede8dfbaf52617ba24d403d173fd58e0ed91817
danil1ned <144240012+danil1ned@users.noreply.github.com>
2024-05-30 14:44:46 +0300

    Changed lock state visibility

--- android/src/main/java/io/wazo/callkeep/VoiceConnectionService.java
+++ android/src/main/java/io/wazo/callkeep/VoiceConnectionService.java
@@ -385,7 +385,7 @@ public class VoiceConnectionService extends ConnectionService {
       channelName,
       NotificationManager.IMPORTANCE_MAX
     );
-    chan.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+    chan.setLockscreenVisibility(Notification.VISIBILITY_PUBLIC);
     NotificationManager manager = (NotificationManager) getSystemService(
       Context.NOTIFICATION_SERVICE
     );
@@ -399,7 +399,7 @@ public class VoiceConnectionService extends ConnectionService {
     notificationBuilder
       .setOngoing(true)
       .setSilent(true)
-      .setVisibility(1)
+      .setVisibility(Notification.VISIBILITY_PUBLIC)
       .setContentTitle(foregroundSettings.getString("notificationTitle"))
       .setPriority(NotificationManager.IMPORTANCE_MAX)
       .setCategory(Notification.CATEGORY_SERVICE);

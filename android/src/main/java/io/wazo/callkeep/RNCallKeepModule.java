2cd9e50ba1bfaee54e311258741d791aa89e755a
danil1ned <144240012+danil1ned@users.noreply.github.com>
2024-06-03 13:05:45 +0300

    Changed context flags

--- android/src/main/java/io/wazo/callkeep/RNCallKeepModule.java
+++ android/src/main/java/io/wazo/callkeep/RNCallKeepModule.java
@@ -1461,32 +1461,20 @@ public class RNCallKeepModule
       .getPackageManager()
       .getLaunchIntentForPackage(packageName)
       .cloneFilter();
-    Activity activity = getCurrentReactActivity();
-    boolean isOpened = activity != null;
-    Log.d(
-      TAG,
-      "[RNCallKeepModule] backToForeground, app isOpened ?" +
-      (isOpened ? "true" : "false")
+
+    focusIntent.addFlags(
+      Intent.FLAG_ACTIVITY_NEW_TASK |
+      Intent.FLAG_ACTIVITY_REORDER_TO_FRONT |
+      Intent.FLAG_ACTIVITY_SINGLE_TOP
     );
 
-    if (isOpened) {
-      focusIntent.addFlags(
-        Intent.FLAG_ACTIVITY_REORDER_TO_FRONT |
-        WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
-        WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD |
-        WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
-      );
-      activity.startActivity(focusIntent);
-    } else {
-      focusIntent.addFlags(
-        Intent.FLAG_ACTIVITY_NEW_TASK |
-        WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
-        WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD |
-        WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
-      );
+    focusIntent.addFlags(
+      WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED +
+      WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD +
+      WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
+    );
 
-      getReactApplicationContext().startActivity(focusIntent);
-    }
+    getReactApplicationContext().startActivity(focusIntent);
   }
 
   public static void onRequestPermissionsResult(
